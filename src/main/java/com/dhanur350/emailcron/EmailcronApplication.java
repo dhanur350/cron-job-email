efa8d8f2219e8e178cebd8aec0a2b4004a415445
dhanur350 <dhanur297@gmail.com>
2026-03-31 22:04:58 +0530

    initial commit

---
+++ src/main/java/com/dhanur350/emailcron/EmailcronApplication.java
@@ -0,0 +1,13 @@
+package com.dhanur350.emailcron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailcronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailcronApplication.class, args);
+	}
+
+}
